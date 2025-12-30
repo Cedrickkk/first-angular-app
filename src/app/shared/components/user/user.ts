c0b18047632427634217c261870469cf7e2820b7
hulaanmo <inhog.cedrick@gmail.com>
2025-12-30 15:59:16 +0800

    feat(user): enhance user component with button and image handling
    - Added ZardButtonComponent for user selection
    - Implemented image path getter for dynamic image loading
    - Updated USERS_MOCK with new user data
    - Modified user template to include button and avatar

--- src/app/shared/components/user/user.ts
+++ src/app/shared/components/user/user.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
 import { ZardAvatarComponent } from '@/shared/components/avatar/avatar.component';
 import { USERS_MOCK } from '@/shared/data/user.mock';
+import { ZardButtonComponent } from '@/shared/components/button/button.component';
 
 @Component({
   selector: 'app-user',
-  imports: [ZardAvatarComponent],
+  imports: [ZardAvatarComponent, ZardButtonComponent],
   templateUrl: './user.html',
 })
 export class User {
-  randomUser = USERS_MOCK[Math.floor(Math.random() * USERS_MOCK.length)];
+  public randomUser = USERS_MOCK[Math.floor(Math.random() * USERS_MOCK.length)];
+
+  /**
+   * Using getters for computed values
+   *
+   */
+  public get imagePath() {
+    return '/images/' + this.randomUser.image;
+  }
+
+  public onSelectUser() {
+    this.randomUser = USERS_MOCK[Math.floor(Math.random() * USERS_MOCK.length)];
+  }
 }
--- src/app/shared/data/user.mock.ts
+++ src/app/shared/data/user.mock.ts
@@ -3,17 +3,17 @@ import { User } from '@/shared/models/user';
 export const USERS_MOCK: User[] = [
   {
     id: '1',
-    name: 'Test 1',
-    image: '/images/cropped-2560-1440-13170191.png',
+    name: 'Muichiro Tokito',
+    image: 'cropped-2560-1440-13170191.png',
   },
   {
     id: '2',
-    name: 'Test 2',
-    image: '/images/cropped-2560-1440-968167.png',
+    name: 'Chihiro Ogino',
+    image: 'cropped-2560-1440-968167.png',
   },
   {
     id: '3',
-    name: 'Test 3',
-    image: '/images/wallpaperflare.jpg',
+    name: 'Torii Gate',
+    image: 'wallpaperflare.jpg',
   },
 ];
